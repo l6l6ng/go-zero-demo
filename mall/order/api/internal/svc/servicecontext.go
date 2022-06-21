ccd6faf4803cb345cc6cbb79c70fd8e14bc9fdbd
liulong@psylife.cc <dfsd534@qq.com>
2022-06-21 22:36:03 +0800

    go-zero-demo

---
+++ mall/order/api/internal/svc/servicecontext.go
@@ -0,0 +1,19 @@
+package svc
+
+import (
+	"github.com/l6l6ng/go-zero-demo/mall/order/api/internal/config"
+	"github.com/l6l6ng/go-zero-demo/mall/user/rpc/user"
+	"github.com/zeromicro/go-zero/zrpc"
+)
+
+type ServiceContext struct {
+	Config  config.Config
+	UserRpc user.User
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config:  c,
+		UserRpc: user.NewUser(zrpc.MustNewClient(c.UserRpc)),
+	}
+}
---
+++ mall/user/rpc/internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "github.com/l6l6ng/go-zero-demo/mall/user/rpc/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
